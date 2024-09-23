ba290b13781bef77d818bef30c40ce7f75d482a6
unknown <muhammaddarajat80@gmail.com>
2024-09-23 18:54:09 +0700

    abcd

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+
+        $user = [
+            'id' =>1,
+            'name'=> 'darajat',
+            'email'=> 'darajat@gmail.com',
+            'password'=> \bcrypt('darajat123'),
+            'kategori_id' =>2,
+        ];
+        User::insert($user);
+    }
+    
+}
